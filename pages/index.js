e373d51b07bd50771bb7f77c1a863f0413e0e4e3
jungjihyoun <jihyoun0602@gmail.com>
2022-06-04 14:05:41 +0900

    feat: 원페이지 스크롤링 추가

--- pages/index.js
+++ pages/index.js
@@ -1,15 +1,17 @@
 import { useRef, useEffect } from "react";
+import { FullPage, Slide } from 'react-full-page';
 import Main from 'component/main/Main'
 import Overview from 'component/overview/Overview'
 import Background from "../component/background/Background";
 
 
 export default function Home() {
+
   return (
-    <div>
-        <Main />
-        <Overview />
-        <Background />
-    </div>
+    <FullPage duration={1700} controls controlsProps={{className: 'controls'}}>
+      <Slide><Main /></Slide>
+      <Slide><Overview /></Slide>
+      <Slide><Background /></Slide>
+    </FullPage>
   )
 }
